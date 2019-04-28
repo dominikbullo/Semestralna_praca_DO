ff455db44cbd3b06bcf31b7d35d0e04f862fb066
Dominik <dominobullo@gmail.com>
2019-04-28 15:53:18 +0200

    Should be OK, maybe some refactoring

--- main.py
+++ main.py
@@ -23,40 +23,38 @@ def load_data():
     return data
 
 
-def check_min_weight(item):
-    if item[3] == True:
-        return item[0]
-    return False
-
-
 def main():
     data = load_data()
 
-    # test = [sum(x) for x in zip(*data)]
     sum_of_weights = sum(i for i, _, k, _ in data if k)
     sum_of_costs = sum(j for _, j, k, _ in data if k)
+    number_of_items = len([k for _, _, k, _ in data if k])
+    print(f"UHF on start is: {sum_of_costs}")
 
-    while sum_of_weights > K and len(data) > r:
-        # TODO: vytvor funkciu ako kluc hldania, vzdy prehladavaju, len tie čo maju true
-        min_weight = min(data, key=lambda t: check_min_weight(t))
-        min_weight_index = data.indexá(min_weight)
+    while sum_of_weights > K and number_of_items > r:
+        min_weight = min((x for x in data if x[2]), key=lambda x: x[0])
+        min_weight_index = data.index(min_weight)
 
         if sum_of_weights - min_weight[0] < K:
             # i have sorted data, that mean, i cant find nothing lower than this values
             break
 
-        print(f'Removing element {min_weight} with minimum weight {min_weight[0]} on index {min_weight_index}')
+        # print(f'Removing element {min_weight} with minimum weight {min_weight[0]} on index {min_weight_index}')
         data[min_weight_index][2] = False
-        sum_of_weights = sum(i for i, _, k, _ in data if k)
-        sum_of_costs = sum(j for _, j, k, _ in data if k)
-        print(f'Sum of weights is: {sum_of_weights}\n'
-              f'Sum_of_costs is: {sum_of_costs}\n'
-              f'Number of items in: {len(data)}\n'
-              f'HUF is {sum_of_costs}')
+        sum_of_weights -= data[min_weight_index][0]
+        sum_of_costs -= data[min_weight_index][1]
+        number_of_items -= 1
+        print(f'HUF is {sum_of_costs}, removed: {min_weight[0]}/{min_weight[1]}')
 
     print(f'SOLUTION:\n'
+          f'HUF is {sum_of_costs}\n'
+          f'Check HUF is {sum(j for _, j, k, _ in data if k)}\n'
+          f'Number_of_items is {number_of_items}\n'
+          f'Check Number_of_items is {len([k for _, _, k, _ in data if k])}\n'
           f'Sum_of_weights is {sum_of_weights}\n'
-          f'Sum_of_costs is {sum_of_costs}')
+          f'Check Sum_of_weights is {sum(i for i, _, k, _ in data if k)}\n'
+          f'Sum_of_costs is {sum_of_costs}\n'
+          f'Sum_of_costs is {sum(j for _, j, k, _ in data if k)}\n')
 
     data.sort(key=lambda t: t[3])
     print(data)
