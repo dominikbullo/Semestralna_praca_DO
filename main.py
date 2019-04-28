0e0cb31ffe6af761f2c943c00240c284688602d1
Dominik <dominobullo@gmail.com>
2019-04-28 16:00:24 +0200

    adding TODO's

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def load_data():
 
     # sort for better search
     data.sort(key=lambda t: t[0])
+
     return data
 
 
@@ -35,17 +36,19 @@ def main():
         min_weight = min((x for x in data if x[2]), key=lambda x: x[0])
         min_weight_index = data.index(min_weight)
 
+        # i have sorted data, that mean, i cant find nothing lower than this values
         if sum_of_weights - min_weight[0] < K:
-            # i have sorted data, that mean, i cant find nothing lower than this values
             break
 
-        # print(f'Removing element {min_weight} with minimum weight {min_weight[0]} on index {min_weight_index}')
         data[min_weight_index][2] = False
         sum_of_weights -= data[min_weight_index][0]
         sum_of_costs -= data[min_weight_index][1]
         number_of_items -= 1
         print(f'HUF is {sum_of_costs}, removed: {min_weight[0]}/{min_weight[1]}')
 
+    data.sort(key=lambda t: t[3])
+
+    # TODO checks
     print(f'SOLUTION:\n'
           f'HUF is {sum_of_costs}\n'
           f'Check HUF is {sum(j for _, j, k, _ in data if k)}\n'
@@ -54,9 +57,9 @@ def main():
           f'Sum_of_weights is {sum_of_weights}\n'
           f'Check Sum_of_weights is {sum(i for i, _, k, _ in data if k)}\n'
           f'Sum_of_costs is {sum_of_costs}\n'
-          f'Sum_of_costs is {sum(j for _, j, k, _ in data if k)}\n')
+          f'Check Sum_of_costs is {sum(j for _, j, k, _ in data if k)}\n')
 
-    data.sort(key=lambda t: t[3])
+    # TODO write into file
     print(data)
 
 
